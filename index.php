4e6dcc4365cbea3e0e48a164a8d01d4198b16a5f
johannranudd <33978097+johannranudd@users.noreply.github.com>
2022-04-08 20:46:27 +0200

    dsfgdfg

--- index.php
+++ index.php
@@ -4,8 +4,14 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="./css/style.css" />
+    <!-- <link rel="stylesheet" href="./css/style.css" /> -->
     <title>Document</title>
+    <style>
+      .redClass {
+        color: green;
+        }
+
+    </style>
   </head>
   <body>
     <h1>this is plain html</h1>
--- js/script.js
+++
@@ -1,6 +0,0 @@
-const btn = document.querySelector('button');
-const headline = document.querySelector('h1');
-btn.addEventListener('click', () => {
-  //   headline.classList.add('redClass');
-  headline.style.color = 'red';
-});
