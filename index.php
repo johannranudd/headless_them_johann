f61afe5dbb525e24f4864004d03a9e4a5995bac9
johannranudd <33978097+johannranudd@users.noreply.github.com>
2022-04-08 20:11:11 +0200

    testing JS and CSS

--- index.php
+++ index.php
@@ -4,12 +4,16 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./css/index.styles.css" />
     <title>Document</title>
   </head>
   <body>
     <h1>this is plain html</h1>
-    <?php 
+    <button>click</button>
+    <!-- <?php 
     include ("test.php")
-    ?>
+    ?> -->
+
+    <script src="./js/script.js"></script>
   </body>
 </html>
---
+++ js/script.js
@@ -0,0 +1,5 @@
+const btn = document.querySelector('button');
+const headline = document.querySelector('h1');
+btn.addEventListener('click', () => {
+  headline.classList.add('redClass');
+});
