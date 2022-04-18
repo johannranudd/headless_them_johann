7f74b7a14df084ddaaaa777b55197d2e0fe10962
johannranudd <33978097+johannranudd@users.noreply.github.com>
2022-04-18 09:42:09 +0200

    removing bloat

--- index.php
+++ index.php
@@ -4,31 +4,12 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <!-- <link rel="stylesheet" href="./css/style.css" /> -->
-    <title>Document</title>
-    <style>
-      .redClass {
-        color: green;
-        }
-
-    </style>
+    <link rel="stylesheet" href="./style.css" />
+    <title>Headless-CMS</title>
+    
   </head>
   <body>
-    <h1>this is plain html</h1>
-    <button>click</button>
-    <?php 
-    include ("test.php")
-    ?>
-    
-
-    <script type="text/javascript">
-      const btn = document.querySelector('button');
-      const headline = document.querySelector('h1');
-      btn.addEventListener('click', () => {
-          headline.classList.add('redClass');
-        // headline.style.color = 'red';
-      });
-    </script>
+  
   </body>
 </html>
 
